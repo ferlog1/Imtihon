dbb88c703097582fd68d62ce92438543591a9d2d
bobj0nov <prosto.akkaunt.n2@gmail.com>
2026-05-25 11:48:36 +0500

    Add files via upload

---
+++ lesson 31/styles.js
@@ -0,0 +1,79 @@
+// let son1 = Number(prompt("Birinchi kunni kiritng"));
+// let son2 = Number(prompt("Ikkinchi son kiriting"));
+// let amal = prompt("Amal kiriting");
+
+// switch (amal) {
+//   case "+":
+//     console.log("Natija:", son1 + son2);
+//     break;
+
+//   case "-":
+//     console.log("Natija:", son1 - son2);
+//     break;
+
+//   case "*":
+//     console.log("Natija:", son1 * son2);
+//     break;
+
+//   case "/":
+//     console.log("Natija:", son1 / son2);
+//     break;
+
+//   default:
+//     console.log("Bunday amal mavjud emas");
+// }
+
+// let foydalanuvchi = prompt("Rolingizni kiriting");
+// switch (foydalanuvchi){
+//     case "admin":
+//         console.log("Tizimni to'liq boshqarish huquqi")
+//         break;
+//          case "muharir":
+//         console.log("Maqolalarni yozish va tahrirlash huquqi")
+//         break;
+//          case "mehmon":
+//         console.log("Faqat o'qish huquqi")
+//         break;
+//         default: console.log( "Noma'lum rol");
+        
+// }
+
+
+
+
+
+
+
+
+
+
+
+// let parol = prompt("Parol kiriting")
+// let natija = (parol.length <= 8)? "Parol juda qisqa!" : "Parol qabul qilindi";
+// console.log(natija)
+
+
+// let tezlik = Number(prompt("Tezlikni kiriting"))
+// let natija = (tezlik >= 70)?"Tezlikni oshirdingiz! Jarima" : "Harakatlanish me'yorda"
+// console.log(natija)
+
+
+
+
+
+let yosh =  prompt("Yoshingizni kiritng")
+let aniqla = (yosh <= 12)? "Bola":
+             (yosh >= 12 & yosh <= 18 )? "O'smir": "Katta";
+console.log(aniqla)
+             
+
+
+
+
+
+
+
+
+
+
+
